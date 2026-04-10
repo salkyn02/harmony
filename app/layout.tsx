3b31f96014097ac2939cacf9be13bdc5bd20d469
salkyn02 <skaiypova02@icloud.com>
2026-04-10 15:51:38 -0700

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
+import "./globals.css"
 import Link from "next/link";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -6,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <Link href={"/"}>
           <h1>Harmony</h1>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,8 @@ export default function LoginPage() {
 
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <h2>Login</h2>
       <form
         onSubmit={async (event) => {
